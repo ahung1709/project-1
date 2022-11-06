923013207a7b82d78ca223bce57edd0675deac6d
Aaron Hung <ahungwork@yahoo.ca>
2022-11-05 23:59:19 -0700

    create and style gameboard and scoreboard

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 console.log("Connected!")
+
+/*----- constants -----*/
+const colors = {
+    'null':'lightgray', 
+    '1': 'black', // for black player 
+    '-1': 'red' // for red player
+}
+
+/*----- app's state (variables) -----*/
+let board = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]
+
+
+
+/*----- cached element references -----*/
+let gameBoardEl = document.getElementById('gameboard')
+gameBoardEl.addEventListener('click', handleClick)
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
